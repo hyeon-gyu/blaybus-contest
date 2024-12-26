9df54b051b1f81d19cdb3b04204c9bda6b849142
hyeon-gyu <hyungue729@naver.com>
2024-12-26 13:29:38 +0900

    first commit

---
+++ src/main/java/contest/blaybus/BlaybusApplication.java
@@ -0,0 +1,13 @@
+package contest.blaybus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlaybusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlaybusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/contest/blaybus/BlaybusApplicationTests.java
@@ -0,0 +1,13 @@
+package contest.blaybus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlaybusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
