7f7a3220e8a070a737bbce3fc357a4756c432368
hyeon-gyu <hyungue729@naver.com>
2024-12-26 17:29:42 +0900

    feature| swagger 적용

---
+++ src/main/java/contest/blaybus/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package contest.blaybus.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Blaybus 앱 개발 경진대회") // API의 제목
+                .description("Backend Swagger") // API에 대한 설명
+                .version("1v"); // API의 버전
+    }
+}
