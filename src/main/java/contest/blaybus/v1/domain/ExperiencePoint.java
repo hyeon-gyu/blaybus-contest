633b3c3ba0e8d159296deb11dc5d9835ac15daee
hyeon-gyu <hyungue729@naver.com>
2025-01-09 15:32:27 +0900

    issue#6 경험치, 경험치 기록, 직군 엔티티 작성

---
+++ src/main/java/contest/blaybus/v1/domain/ExperiencePoint.java
@@ -0,0 +1,45 @@
+package contest.blaybus.v1.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ExperiencePoint {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int year; // 년도
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member; // 멤버와 1대다 관계 예상
+
+    private long totalExp; // 해당 년도 누적 경험치
+    private long half1Eval; // 상반기 인사평가 점수
+    private long half2Eval; // 하반기 인사평가 점수
+    private long jobQuestEval; // 직무 퀘스트 점수
+    private long leaderQuestEval; // 리더 퀘스트 점수
+    private long companyProjectScore; // 전사 프로젝트 점수
+
+    // 연관관계 편의 메소드
+    public void assignMember(Member member) {
+        if(this.member != null) {
+            this.member.getExperiencePoints().remove(this);
+        }
+        this.member = member; // exp -> member
+        if (!member.getExperiencePoints().contains(this)) {
+            member.getExperiencePoints().add(this); // member -> exp
+        }
+    }
+}
