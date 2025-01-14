ec231bd7ab67df977121e3ac5d1fa4e86b5c4397
FromKyoung <junhyoung@JH-MacBook-Air.local>
2025-01-15 04:49:41 +0900

    issue#19 인증, 인가 실패 핸들러 추가

---
+++ src/main/java/contest/blaybus/v1/security/handler/AuthAccessDeniedHandler.java
@@ -0,0 +1,32 @@
+package contest.blaybus.v1.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * 인가가 실패했을 때
+ * 권한이 부족할때
+ */
+
+@Component
+public class AuthAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        objectMapper.writeValue(response.getWriter(), "권한이 부족합니다.");
+    }
+}
