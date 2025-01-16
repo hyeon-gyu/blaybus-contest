253f2a75ec6e5e7ee2d6a14bafab56590800413c
hyeon-gyu <hyungue729@naver.com>
2025-01-17 07:06:11 +0900

    fix CORS 디버그

--- src/main/java/contest/blaybus/v1/config/SwaggerConfig.java
+++ src/main/java/contest/blaybus/v1/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +20,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(getOpenAPIInfo())
                 .components(getOpenAPIComponents())
+                .addServersItem(new Server().url("https://superblaybus.store"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 
