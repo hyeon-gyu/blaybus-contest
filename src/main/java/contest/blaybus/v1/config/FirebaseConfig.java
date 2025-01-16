27fe07330c5f209d85a7cdf7802016f215cd1da9
hyeon-gyu <hyungue729@naver.com>
2025-01-16 20:55:43 +0900

    fix FCM 테스트

--- src/main/java/contest/blaybus/v1/config/FirebaseConfig.java
+++ src/main/java/contest/blaybus/v1/config/FirebaseConfig.java
@@ -3,28 +3,40 @@ package contest.blaybus.v1.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ResourceUtils;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
-    public void init(){
-        try{
-            InputStream serviceAccount = new ClassPathResource("firebase_service_key.json").getInputStream();
+    public void init() {
+        try {
+//            InputStream serviceAccount = new ClassPathResource("firebase_service_key.json").getInputStream();
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .build();
+
+            FileInputStream serviceAccount =
+                    new FileInputStream("./src/main/resources/firebase_service_key.json");
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
-
             if (FirebaseApp.getApps().isEmpty()) { // FirebaseApp이 이미 초기화되어 있지 않은 경우에만 초기화 실행
                 FirebaseApp.initializeApp(options);
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
