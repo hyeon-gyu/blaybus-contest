f41b0aa3e5e7bf12efb9af3e5e1141b78217ab97
hyeon-gyu <hyungue729@naver.com>
2025-01-17 10:53:08 +0900

    fix CORS 디버그

--- src/main/java/contest/blaybus/v1/config/CorsConfig.java
+++ src/main/java/contest/blaybus/v1/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://superblaybus.store")
+                .allowedOrigins("http://localhost:3000", "https://superblaybus.store","https://dohi.netlify.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
